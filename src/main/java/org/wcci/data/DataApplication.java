a34ce8c4643b7e2701bae6d4286ab0c073976909
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-08-02 22:49:55 -0400

    Adding workflow yml

---
+++ src/main/java/org/wcci/data/DataApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataApplication.class, args);
+	}
+
+}
